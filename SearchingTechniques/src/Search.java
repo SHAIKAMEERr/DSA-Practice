dc6126510811e4e13b19e59b8c3a17b12e705c7c
SHAIK AMEER <shaikameerjann@gmail.com>
2025-03-07 18:13:17 +0530

    Linear Search Practice

--- SearchingTechniques/src/Search.java
+++ SearchingTechniques/src/Search.java
@@ -6,6 +6,7 @@ public class Search
     public static void main(String[] args)
     {
         // Linear Search in an Array
+        //Code related to Linear Search in an Array
         int[] arr = {12, 32,14, 61,79, 11, 91, 12, 37, 12};
 
         System.out.println("Find The First Index of a Target Element In An Array: "+LinearSearch
@@ -18,6 +19,7 @@ public class Search
         System.out.println();
 
         //linear Search in a String
+        //code related to Linear Search in a String
         String str = "PracticingJavaPrograms";
 
         System.out.println("Find The First Index of a Target Element In An String: "+LinearSearchInString
@@ -30,6 +32,7 @@ public class Search
         System.out.println();
 
         //Linear Search in a range
+        //code related to Linear Search in a Range
         int[] array = {12, 34, 56, 67, 78, 98, 90, 13, 22};
         System.out.println("Searching element in a Range: "+SearchInTheRange
                 .searchInARange(array, 1, 4, 56));
@@ -37,6 +40,7 @@ public class Search
         System.out.println();
 
         //find Minimum and maximum in an array
+        //code related to find Minimum and Maximum in an Array
         int[] minMax = {11, 22, 33, 44, 55, 66, 77, 88, 99};
         System.out.println("Find Minimum element in an Array: "+ FindMinimunAndMaximum.findMinimum(minMax));
         System.out.println("Find Maximum element in an Array: "+ FindMinimunAndMaximum.findMaximum(minMax));
@@ -44,6 +48,7 @@ public class Search
         System.out.println();
 
         //Linear search in a 2-D Array
+        //code related to Linear Search in a 2-D Array
         int[][] nums =
                 {{12, 13, 14},
                  {15, 16, 17},
